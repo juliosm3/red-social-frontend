0fb2c9aec7352ca891e5372911e7eb311ca14928
juliosm3 <tuemailexample.com>
2025-04-30 11:28:18 +0200

    archivos modificados

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,17 +1,20 @@
 import axios from 'axios';
 
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: API_BASE,
 });
 
-instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+instance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
 
 export default instance;
